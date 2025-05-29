b8685f1bfaf0415641b70004e933b70be41d6480
Ayush Srivastava <154739711+AYUSH15-28@users.noreply.github.com>
2025-05-29 16:43:06 +0530

    vercel

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-import express from 'express'
-const app = express();
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const PORT =  3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const host = '127.0.0.1';
+const port = 3000;
 
 app.set('view engine', 'ejs');
-app.set('views',path.join(__dirname, 'views'));
-app.get('/my',(req,res)=>{
-    res.send('This is my website');
-})
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'EJS Demo',
-    heading: 'Welcome to My Website', 
-    message:'This is a simple EJS template rendering example.',
-    users: [
-      { name: 'Alice', email: 'alice@example.com' },
-      { name: 'Bob', email: 'bob@example.com' },
-      { name: 'Charlie', email: 'charlie@example.com' }
-    ]
-  });
+    res.render('index', { title: 'welcome to EJS Page!' });
 });
-app.listen(PORT,()=>{
-    console.log(`Server is running on the port${PORT}`);
-})
+
+app.listen(port, host, () => {
+    console.log(`server is running on http://${host}:${port}`);
+});
